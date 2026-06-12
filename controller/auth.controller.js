13154ea1f4d0ae0c40c9d44a85e03e200e2d8476
ankit8 <ankitbummerkar8@gmail.com>
2026-06-12 18:43:01 +0530

    deploying final2

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -22,7 +22,7 @@ export const signup = async (req, res) => {
 
     const upiId = `${name.toLowerCase().replace(/\s/g, "")}@gratitudewallet`;
 
-    const paymentUrl = `http://localhost:5173/pay/${upiId}`;
+    const paymentUrl = `phone-pay-clone-virid.vercel.app/pay/${upiId}`;
 
     const qrCode = await QRCode.toDataURL(paymentUrl);
 
